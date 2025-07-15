49d9d7b285734a6d2f11dd64d86486f0b624e917
Vishal <vishal.n.nsk123@gmail.com>
2025-07-15 11:21:02 +0530

    menu toggle added
    nav buttons removed and menu dropdown added for mobile version

--- nav.js
+++ nav.js
@@ -24,3 +24,10 @@ if (backToTopBtn) {
     }
   });
 }
+
+const hamburger = document.getElementById('hamburger');
+const mobileNav = document.getElementById('mobileNav');
+
+hamburger.addEventListener('click', () => {
+  mobileNav.style.display = mobileNav.style.display === 'flex' ? 'none' : 'flex';
+});
